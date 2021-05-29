bd03acb93386709f6782961884c25177f6e3cc4e
Adnan <muhammad.adnan.rizqu@gmail.com>
2021-05-29 19:35:46 +0700

    handle user input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,20 +1,38 @@
+import { useRef } from 'react';
+
 import classes from './TodoForm.module.css';
 
-function TodoForm() {
+function TodoForm(props) {
+  const titleInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const todoData = {
+      title: enteredTitle,
+      description: enteredDescription,
+    };
+  }
+
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor="title">Title</label>
-        <input type="text" required id="title"/>
+        <input type="text" required id="title" ref={titleInputRef}/>
       </div>
 
       <div className={classes.control}>
         <label htmlFor="description">Description</label>
-        <textarea type="text" required id="description" rows="5"/>
+        <textarea type="text" required id="description" rows="5" ref={descriptionInputRef}/>
       </div>
 
       <div className={classes.actions}>
-        <button>Add Meetup</button>
+        <button>Add Todo</button>
       </div>
     </form>
   )
